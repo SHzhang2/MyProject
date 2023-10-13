455df1848ec5769d5edfac1bf7ed6baf257df089
zhang <2267331731@qq.com>
2023-10-13 16:52:35 +0800

    First commit.

---
+++ app/src/main/java/com/example/myplain/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.myplain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
